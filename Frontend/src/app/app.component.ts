e37bd340794ffd1bc62327855d8c610dda7a2ac8
cotta0322 <cotta0322@gmail.com>
2020-08-03 12:41:11 +0000

    FrontEndのレイアウト対応

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+interface ChatDataType {
+  user: 'other' | 'myself';
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Frontend';
+  @ViewChild('dispArea') dispArea: ElementRef;
+
+  SAMPLE_DATA: ChatDataType[] = [
+    { user: 'other', message: 'サンプルメッセージ' },
+    { user: 'myself', message: 'サンプルメッセージ\nサンプルデータ' },
+    { user: 'other', message: 'サンプルメッセージ' },
+    { user: 'myself', message: 'サンプルメッセージ' },
+    { user: 'other', message: 'サンプルメッセージ' },
+  ];
+
+  messageData = this.SAMPLE_DATA;
+
+  submit(message: string) {
+    this.messageData.push({
+      user: 'myself',
+      message,
+    });
+  }
+
+  ngAfterViewChecked() {
+    this.dispAreaToBottom();
+  }
+
+  dispAreaToBottom() {
+    this.dispArea.nativeElement.scrollTop = this.dispArea.nativeElement.scrollHeight;
+  }
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
